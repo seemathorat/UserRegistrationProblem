d83cb665f977308e66fb9536b5997e011882aa26
seema thorat <seemagade97@gmail.com>
2021-03-01 10:38:14 +0530

    First Name Validation Done

---
+++ regex.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class regex {
+	
+	public void firstName() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Name: ");
+		String firstName = sc.next();
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(firstName);
+		boolean result = matcher.matches();
+		if(result == true)
+			System.out.println("First Name is Valid...");
+		else
+			System.out.println("First Name is not valid...");
+	}
+
+	public static void main(String[] args) {
+		regex regex = new regex();
+		regex.firstName();
+	}
+
+}
